4fa502dce71f36adae07d042589b846b9665e56d
Marianna <mddobal@gmail.com>
2019-07-13 14:39:10 -0300

    fixed

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ const ctx = canvas.getContext('2d');
 ctx.lineWidth = 3;
 
 
+
+ctx.strokeStyle = '#fff';
+
+
+// //circulo do meio
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.arc(400, 200, 50, 0, 2 * Math.PI, true);
+ctx.stroke();
+ctx.fillStyle = '#ff6600'; //laranja
+ctx.fill();
+ctx.closePath();
+
+// //linha do meio
+ctx.beginPath();
+ctx.moveTo(400, 0);
+ctx.lineTo(400, 400);
+ctx.stroke();
+ctx.closePath();
+
 //semi circulo cheio da esquerda
 ctx.beginPath();
 ctx.arc(150, 200, 50, Math.PI/2, 3*Math.PI/2, true);
-ctx.strokeStyle = '#fff';
 ctx.stroke();
 ctx.closePath();
 
 // //semicirculo cheio direita
 ctx.beginPath();
 ctx.arc(650, 200, 50, 3*Math.PI/2, Math.PI/2, true);
-ctx.strokeStyle = '#fff';
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
 ctx.arc(50, 200, 150, Math.PI/2, 3*Math.PI/2, true);
-ctx.strokeStyle = '#fff';
 ctx.stroke();
 ctx.closePath();
 
@@ -30,7 +47,7 @@ ctx.arc(750, 200, 150, 3*Math.PI/2, Math.PI/2, true);
 ctx.stroke();
 ctx.closePath();
 
-// //linhas das bordas dos circulos até os cantos da quadra
+//linhas das bordas dos circulos até os cantos da quadra
 ctx.beginPath();
 ctx.moveTo(750, 350);
 ctx.lineTo(800, 350);
@@ -59,9 +76,9 @@ ctx.closePath();
 ctx.strokeRect(-1, 150, 150, 100);
 ctx.strokeRect (-1, 130, 150, 140);
 
-ctx.fillStyle = '#ff6600';
 
 ctx.fillRect(-2, 151, 149, 98);
+
 
 // //retangulos direita
 ctx.strokeRect(650, 150, 151, 100);
@@ -82,6 +99,8 @@ ctx.lineTo(50, 200);
 ctx.stroke();
 ctx.closePath();
 
+
+
 // //circulo preenchido esq
 ctx.beginPath();
 ctx.arc(53, 200, 5, 0, Math.PI * 2, true);
@@ -89,6 +108,16 @@ ctx.stroke();
 ctx.fillStyle = '#fff';
 ctx.fill();
 ctx.closePath();
+
+
+//circulo preenchido dir
+ctx.beginPath();
+ctx.arc(747, 200, 5, 0, Math.PI * 2, true);
+ctx.stroke();
+ctx.fillStyle = '#fff';
+ctx.fill();
+ctx.closePath();
+
 
 // //linha direita vert
 ctx.beginPath();
@@ -102,36 +131,10 @@ ctx.beginPath();
 ctx.lineWidth = 3;
 ctx.moveTo(750, 200);
 ctx.lineTo(760, 200);
-ctx.strokeStyle = '#fff';
-ctx.stroke();
-ctx.closePath();
-
-// //circulo preenchido dir
-ctx.beginPath();
-ctx.arc(747, 200, 5, 0, endAngle, true);
-ctx.stroke();
-ctx.fillStyle = '#fff';
-ctx.fill();
-ctx.closePath();
-
-// //circulo do meio
-ctx.beginPath();
-ctx.lineWidth = 5;
-ctx.arc(400, 200, 50, 0, 2 * Math.PI, true);
-ctx.stroke();
-ctx.fillStyle = '#ff6600';
-ctx.fill();
-ctx.closePath();
-
-// //linha do meio
-ctx.beginPath();
-ctx.moveTo(400, 0);
-ctx.lineTo(400, 400);
 ctx.stroke();
 ctx.closePath();
 
 
-ctx.lineWidth = 3;
 // //semi circulo pontilhado esquerda
 ctx.beginPath();
 ctx.setLineDash([5, 5]);
@@ -145,6 +148,8 @@ ctx.beginPath();
 ctx.arc(650, 200, 50, 3*Math.PI/2, Math.PI/2);
 ctx.stroke();
 ctx.closePath();
+
+
 
 
 
